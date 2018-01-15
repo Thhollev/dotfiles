10624f24dacd5b87f24c00c81f26cf8557a3d17f
thhollev <thhollev.hollevoet@ugent.be>
2018-01-15 11:45:48 +0100

    update bat noti

--- scripts/battery-noti.sh
+++ scripts/battery-noti.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 while true; do
-	if [[ "$(cat /sys/class/power_supply/BAT0/status)" != "Charging" ]]; then
-		now=$(cat /sys/class/power_supply/BAT0/energy_now)
-		full=$(cat /sys/class/power_supply/BAT0/energy_full)
+	now=$(cat /sys/class/power_supply/BAT0/energy_now 2>/dev/null)
+	full=$(cat /sys/class/power_supply/BAT0/energy_full 2>/dev/null)
 
+	if [ ! -z $now ] && [ ! -z $full ]; then
 		percentage=$(printf %.$2f $(echo "$now / $full * 100" | bc -l))
-
-		if [ $percentage -lt 5 ]; then
-			notify-send -u "critical" "Battery is super low, give me power!"
-		elif [ $percentage -lt 10 ]; then
-			notify-send -u "critical" "Battery is low!"
-		elif [ $percentage -eq 100 ]; then
-			notify-send -u low "Battery fully charged."
+			
+		if [[ "$(cat /sys/class/power_supply/BAT0/status 2>/dev/null)" != "Charging" ]]; then
+			if [ $percentage -lt 5 ]; then
+				notify-send -u "critical" "Battery is super low, give me power!"
+			elif [ $percentage -lt 10 ]; then
+				notify-send -u "critical" "Battery is low!"
+			fi
+		else
+			if [ $percentage -eq 100 ]; then
+				notify-send -u low "Battery fully charged."
+			fi
 		fi
 	fi
-
 	sleep '1m'
 done
